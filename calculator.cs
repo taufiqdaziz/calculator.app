e45cb8dac46397be0ae53d21f365af769595efb1
taufiqdaziz <taufiqdaziz@amikom.ac.id>
2022-03-16 11:01:23 +0700

    calculator

---
+++ calculator.cs
@@ -0,0 +1,22 @@
+class Calculator
+{
+	static void main (string[]args)
+	{
+		int a = 10;
+		int b = 6;
+		
+		console.writeline("hasil penambahan : {0} + {1} = {2}", a, b, penambahn(a, b));
+		console.writeline("hasil pengurangan : {0} - {1} = {2}", a, b, penambahn(a, b));
+		
+		console.writeline("\ntekan enter untuk keluar");
+		console.readkey();
+	}
+	static int penambahan(int a, int b);
+	{
+		return a+b;
+	}
+	static int penambahan(int a, int b);
+	{
+		return a-b;
+	}
+}
